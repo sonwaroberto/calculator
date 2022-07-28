515389caa47cfa0321d5f17c3b3850ca1629a7ef
sonwaboris <sonwaboris123@gmail.com>
2022-07-28 17:58:52 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,77 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {useEffect, useState,useReducer} from "react"
+import ReactDOM from "react-dom/client"
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Plus = () => {
+  const initialState = {result: 0}
+  const reducer = (state,action) => {
+    if (isNaN(number1) || isNaN(number2)) return initialState;
+    // const num1 = number1 || 0
+    // const num2 = number2 || 0
+    switch(action.sign) {
+      case '+':
+        return {result : number1 + number2}
+      case '-': 
+        return {result : number1 - number2}
+      case '*':
+        return {result : number1 * number2}
+      case '/':
+        return {result : number1 / number2}
+      case '^':
+        return {result : Math.pow(number1,number2)}
+      default:
+        return state
+    }
+  } 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const [number1, setNumber1] = useState(0)
+  const [number2, setNumber2] = useState(0)
+  const [sign, setSign] = useState('+')
+  const operators = ['+','-','*','/','^']
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    dispatch({sign,number1,number2})
+  }, [number1, number2,sign])
+  
+  return (
+    <Box sx={{display:"flex",alignItems:"center"}}>
+      <TextField 
+        required 
+        label="number 1" 
+        placeholder="enter number to sum" 
+        type="number"
+        variant="outlined"
+        onChange={(e) => setNumber1(parseFloat(e.target.value))}
+      />  
+      <Select
+        value={sign}
+        label="Sign"
+        onChange={(e) => setSign((e.target.value))}
+      >
+        {operators.map((opr,index)=>{
+          return <MenuItem value={opr} key={index}>{opr}</MenuItem>
+        })}
+      </Select>
+      <TextField 
+        required 
+        label="number 2" 
+        type="number"
+        placeholder="enter number to sum" 
+        variant="outlined"
+        onChange={(e) => setNumber2(parseFloat(e.target.value))}
+      /> 
+      <span>=</span>  
+      <Box component="span" sx={{ p: 2, border: '1px solid rgba(0, 0, 0, 0.23)' }}>
+        {state.result}
+      </Box>
+    </Box>
+
+  )
+}
+
+const root = ReactDOM.createRoot(document.querySelector("#root"))
+root.render(<Plus/>)
